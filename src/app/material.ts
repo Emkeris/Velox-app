9782e52588d4d947417087992cd7823430942a3d
emkeris <emkeris@gmail.com>
2019-03-20 11:16:27 +0000

    something

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule],
+  imports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatFormFieldModule, MatSelectModule],
+  exports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatFormFieldModule, MatSelectModule],
 })
 export class MaterialModule { }
